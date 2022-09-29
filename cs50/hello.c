ea2aa2a724b015f852cda8048ad4f2445791407d
OmShinde <omshinde9898@gmail.com>
2022-09-29 23:27:06 +0530

    In work

--- cs50/hello.c
+++ cs50/hello.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int print_arr(int n[]);
+
 int main(void){
-    printf("Hello, World");
+    int a[10] = {10,20,30,40,60,50,70,80,90,0};
+    print_arr(a);
+    char name[20];
+    printf("Enter Name : ");
+    scanf("%s",name);
+    printf("Hello, %s",name);
     return 0;
 }
+
+int print_arr(int n[]){
+    for(int i; i<10;i++){
+        printf("%i \n",n[i]);
+    }
+    return 0;
+}
+
+struct node
+{
+    int number;
+    struct node* next;
+};
+node;
+
+
